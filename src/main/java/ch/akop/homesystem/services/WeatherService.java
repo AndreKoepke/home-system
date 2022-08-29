f064b37745385344ff79ae5580fa202b901abb2e
Andre Köpke <andre.koepke@sbb.ch>
2022-08-29 18:44:15 +0200

    finish the RollerShutter

--- src/main/java/ch/akop/homesystem/services/WeatherService.java
+++ src/main/java/ch/akop/homesystem/services/WeatherService.java
@@ -1,5 +1,7 @@
 package ch.akop.homesystem.services;
 
+import ch.akop.homesystem.models.CompassDirection;
+import ch.akop.homesystem.services.impl.WeatherServiceImpl;
 import ch.akop.weathercloud.Weather;
 import io.reactivex.rxjava3.core.Flowable;
 
@@ -7,6 +9,10 @@ public interface WeatherService {
 
     Flowable<Weather> getWeather();
 
+    Flowable<WeatherServiceImpl.CurrentAndPreviousWeather> getCurrentAndPreviousWeather();
+
     boolean isActive();
+
+    CompassDirection getCurrentSunDirection();
 
 }
--- src/main/java/ch/akop/homesystem/services/impl/RollerShutterService.java
+++ src/main/java/ch/akop/homesystem/services/impl/RollerShutterService.java
@@ -4,6 +4,7 @@ import ch.akop.homesystem.config.properties.HomeSystemProperties;
 import ch.akop.homesystem.models.devices.actor.RollerShutter;
 import ch.akop.homesystem.services.DeviceService;
 import ch.akop.homesystem.services.WeatherService;
+import ch.akop.weathercloud.Weather;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import javax.annotation.PostConstruct;
 import java.util.NoSuchElementException;
 
 import static ch.akop.weathercloud.light.LightUnit.KILO_LUX;
+import static ch.akop.weathercloud.temperature.TemperatureUnit.DEGREE;
 
 @Service
 @RequiredArgsConstructor
@@ -23,33 +25,43 @@ public class RollerShutterService {
     @PostConstruct
     private void init() {
         homeSystemProperties.getRollerShutters()
-                .forEach(rollerShutterConfig -> {
-                    var rollerShutter = deviceService.findDeviceByName(rollerShutterConfig.getName(), RollerShutter.class)
-                            .orElseThrow(() -> new NoSuchElementException("No rollerShutter named '%s' was found in deviceList.".formatted(rollerShutterConfig.getName())));
-
-                    weatherService.getWeather()
-                            .subscribe(weather -> {
-                                var isRollerShutterOpen = rollerShutter.getCurrentLift() > 50 || rollerShutter.getCurrentTilt() > 50;
-                                // TODO tbd
-                                var isRollerShutterHeadingToSun = true;
-
-                                if (isRollerShutterOpen) {
-                                    // we want to close it when it's getting very bright
-                                    // or when the nights start
-
-                                    if (isRollerShutterHeadingToSun && weather.getLight().isBiggerThan(250, KILO_LUX)) {
-                                        rollerShutter.setLiftAndThenTilt(0, 30);
-                                    } else if (weather.getLight().isSmallerThan(20, KILO_LUX)) {
-                                        rollerShutter.setLiftAndThenTilt(0, 20);
-                                    }
-                                } else {
-                                    // we want to open it, when it is not so bright anymore
-
-                                }
-
-                            });
-
-                });
+                .forEach(rollerShutterConfig -> weatherService.getCurrentAndPreviousWeather()
+                        .subscribe(weather -> {
+                            var currentComparedToPrevious = weather.current().getLight().compareTo(weather.previous().getLight().getAs(KILO_LUX));
+                            if (currentComparedToPrevious < 0) {
+                                itsGettingDarkerOutside(rollerShutterConfig, weather.current());
+                            } else if (currentComparedToPrevious > 0) {
+                                itsGettingBrighterOutside(rollerShutterConfig, weather.current());
+                            }
+                        }));
     }
 
+
+    private void itsGettingBrighterOutside(HomeSystemProperties.RollerShutter config, Weather currentWeather) {
+        var rollerShutter = getRollerShutter(config);
+        var isOpen = rollerShutter.getCurrentLift() > 50 || rollerShutter.getCurrentTilt() > 50;
+
+        if (isOpen
+                && weatherService.getCurrentSunDirection().equals(config.getCompassDirection())
+                && currentWeather.getLight().isBiggerThan(250, KILO_LUX)
+                && currentWeather.getOuterTemperatur().isBiggerThan(15, DEGREE)) {
+            rollerShutter.setLiftAndThenTilt(0, 20);
+        }
+    }
+
+    private void itsGettingDarkerOutside(HomeSystemProperties.RollerShutter config, Weather currentWeather) {
+        var rollerShutter = getRollerShutter(config);
+        var isOpen = rollerShutter.getCurrentLift() > 50 || rollerShutter.getCurrentTilt() > 50;
+
+        if (isOpen && currentWeather.getLight().isSmallerThan(20, KILO_LUX)) {
+            rollerShutter.setLiftAndThenTilt(0, 0);
+        } else if (!isOpen && currentWeather.getLight().isSmallerThan(75, KILO_LUX)) {
+            rollerShutter.setLiftAndThenTilt(100, 100);
+        }
+    }
+
+    private RollerShutter getRollerShutter(HomeSystemProperties.RollerShutter config) {
+        return deviceService.findDeviceByName(config.getName(), RollerShutter.class)
+                .orElseThrow(() -> new NoSuchElementException("No rollerShutter named '%s' was found in deviceList.".formatted(config.getName())));
+    }
 }
