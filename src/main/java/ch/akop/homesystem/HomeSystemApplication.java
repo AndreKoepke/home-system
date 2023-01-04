2c3d4277d0b34f57d38dc34f62210c00a6a367d6
akop <andre.koepke@ppi.de>
2023-01-04 22:18:37 +0100

    Fix config-Finder

--- src/main/java/ch/akop/homesystem/HomeSystemApplication.java
+++ src/main/java/ch/akop/homesystem/HomeSystemApplication.java
@@ -3,7 +3,6 @@ package ch.akop.homesystem;
 import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -14,7 +13,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 @EnableCaching
-@ConfigurationPropertiesScan("ch.akop.homesystem.config.properties")
 public class HomeSystemApplication {
 
     @SneakyThrows
