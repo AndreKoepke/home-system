0b7caf3b3c79707a417d2918913788f8fb7f4bf4
akop <github@akop.online>
2023-05-01 15:41:11 +0200

    order matters

--- backend/src/main/java/ch/akop/homesystem/deconz/DeconzWebsocketListener.java
+++ backend/src/main/java/ch/akop/homesystem/deconz/DeconzWebsocketListener.java
@@ -60,12 +60,14 @@ public class DeconzWebsocketListener implements WebSocket.Listener {
         .ifPresent(config -> {
           var wsUrl = URI.create("ws://%s:%d/ws".formatted(config.getHost(), config.getWebsocketPort()));
 
+          log.info("Try to open a connection to " + wsUrl);
+
           //noinspection ResultOfMethodCallIgnored
           Observable.defer(() -> Observable.fromFuture(getWebSocketCompletableFuture(wsUrl, this)))
-              .subscribeOn(blockingScheduler)
               .retryWhen(origin -> origin
                   .doOnNext(throwable -> log.warn("Connection attempt failed, retrying in 5s."))
                   .delay(5000, TimeUnit.SECONDS))
+              .subscribeOn(blockingScheduler)
               .subscribe(webSocket -> this.webSocket = webSocket);
         });
   }
