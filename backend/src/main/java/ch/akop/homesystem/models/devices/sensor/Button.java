9d9fdc20e31432adf91a3de95e2a62d3b5a6c744
Andre Köpke <andre.koepke@sbb.ch>
2023-06-10 18:09:29 +0200

    fix

--- backend/src/main/java/ch/akop/homesystem/models/devices/sensor/Button.java
+++ backend/src/main/java/ch/akop/homesystem/models/devices/sensor/Button.java
@@ -4,12 +4,16 @@ import ch.akop.homesystem.deconz.rest.State;
 import io.reactivex.rxjava3.subjects.ReplaySubject;
 import io.reactivex.rxjava3.subjects.Subject;
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Button extends Sensor<Button> {
@@ -19,9 +23,15 @@ public class Button extends Sensor<Button> {
 
   @Override
   protected void consumeInternalUpdate(State update) {
-    var updatedBefore = Duration.between(update.getLastupdated().atZone(ZoneOffset.UTC), ZonedDateTime.now()).abs();
-    if (updatedBefore.compareTo(Duration.ofSeconds(30)) < 0) {
-      events$.onNext(update.getButtonevent());
+    try {
+      var lastUpdated = LocalDateTime.parse(update.getLastupdated());
+      var updatedBefore = Duration.between(lastUpdated.atZone(ZoneOffset.UTC), ZonedDateTime.now()).abs();
+      if (updatedBefore.compareTo(Duration.ofSeconds(30)) < 0) {
+        events$.onNext(update.getButtonevent());
+      }
+    } catch (DateTimeParseException e) {
+      log.warn("Button got a update with an invalid timestamp. LastUpdated was '"
+          + update.getLastupdated() + "' for button '" + this.getName() + "' (id: '" + this.getId() + "')");
     }
   }
 }
