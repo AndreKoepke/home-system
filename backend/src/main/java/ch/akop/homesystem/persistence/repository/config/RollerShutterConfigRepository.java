126ae8cb52b590f43b106835aa098b43e0a4c697
Andre Köpke <andre.koepke@sbb.ch>
2023-06-13 18:15:22 +0200

    rewrote rollerShutter logic

--- backend/src/main/java/ch/akop/homesystem/persistence/repository/config/RollerShutterConfigRepository.java
+++ backend/src/main/java/ch/akop/homesystem/persistence/repository/config/RollerShutterConfigRepository.java
@@ -1,11 +1,16 @@
 package ch.akop.homesystem.persistence.repository.config;
 
+import ch.akop.homesystem.models.CompassDirection;
 import ch.akop.homesystem.persistence.model.config.RollerShutterConfig;
+import java.util.stream.Stream;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RollerShutterConfigRepository extends JpaRepository<RollerShutterConfig, String> {
 
+  Stream<RollerShutterConfig> findByCompassDirection(CompassDirection compassDirection);
+
+  Stream<RollerShutterConfig> findRollerShutterConfigByCompassDirectionIsNotNull();
 
 }
--- backend/src/main/java/ch/akop/homesystem/services/impl/RollerShutterService.java
+++ backend/src/main/java/ch/akop/homesystem/services/impl/RollerShutterService.java
@@ -8,6 +8,7 @@ import ch.akop.homesystem.models.devices.actor.RollerShutter;
 import ch.akop.homesystem.persistence.model.config.RollerShutterConfig;
 import ch.akop.homesystem.persistence.repository.config.RollerShutterConfigRepository;
 import ch.akop.homesystem.util.TimeUtil;
+import ch.akop.weathercloud.Weather;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import java.time.Duration;
@@ -30,6 +31,7 @@ import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.e175.klaus.solarpositioning.AzimuthZenithAngle;
+import org.eclipse.microprofile.context.ManagedExecutor;
 
 @Slf4j
 @ApplicationScoped
@@ -39,6 +41,8 @@ public class RollerShutterService {
   private final DeviceService deviceService;
   private final WeatherService weatherService;
   private final RollerShutterConfigRepository rollerShutterConfigRepository;
+  private final TelegramMessageService telegramMessageService;
+  private final ManagedExecutor executor;
 
   private final List<Disposable> disposables = new ArrayList<>();
   private final Map<LocalTime, List<String>> timeToConfigs = new HashMap<>();
@@ -46,24 +50,45 @@ public class RollerShutterService {
 
   @Transactional
   public void init() {
-    initWeatherBasedRollerShutters();
+    disposables.add(weatherService.getWeather()
+        .skip(1)
+        .mergeWith(telegramMessageService.getMessages()
+            .filter(message -> message.startsWith("/calcRollerShutter"))
+            .switchMap(message -> weatherService.getWeather().take(1)))
+        .subscribe(newWeather -> executor.runAsync(() -> handleWeatherUpdate(newWeather))));
     initTimer();
   }
 
-  private void initWeatherBasedRollerShutters() {
-    rollerShutterConfigRepository.findAll()
-        .stream()
-        .filter(config -> config.getCompassDirection() != null)
-        .map(rollerShutterConfig -> weatherService.getCurrentAndPreviousWeather()
-            .subscribe(weather -> {
-              var currentComparedToPrevious = weather.current().getLight().compareTo(weather.previous().getLight().getAs(KILO_LUX));
-              if (currentComparedToPrevious < 0) {
-                itsGettingDarkerOutside(rollerShutterConfig, weather);
-              } else if (currentComparedToPrevious > 0) {
-                itsGettingBrighterOutside(rollerShutterConfig, weather);
-              }
-            }))
-        .forEach(disposables::add);
+  private void handleWeatherUpdate(Weather newWeather) {
+    var newBrightness = newWeather.getLight().getAs(KILO_LUX).intValue();
+
+    if (newBrightness > 150) {
+      var sunDirection = weatherService.getCurrentSunDirection();
+      var compassDirection = resolveCompassDirection(sunDirection);
+
+      if (newWeather.getOuterTemperatur().isSmallerThan(15, DEGREE) && sunDirection.getZenithAngle() > 55) {
+        return;
+      }
+
+      rollerShutterConfigRepository.findByCompassDirection(compassDirection)
+          .map(this::getRollerShutter)
+          .filter(rollerShutter -> rollerShutter.getCurrentLift() > 45)
+          .peek(rollerShutter -> log.info("Weather close for " + rollerShutter.getName() + " because it is too much sun"))
+          .forEach(rollerShutter -> rollerShutter.setLiftAndThenTilt(50, 50));
+
+    } else if (newBrightness < 100 && newBrightness > 10) {
+      rollerShutterConfigRepository.findRollerShutterConfigByCompassDirectionIsNotNull()
+          .map(this::getRollerShutter)
+          .filter(rollerShutter -> rollerShutter.getCurrentLift() < 60 && rollerShutter.getCurrentLift() > 40)
+          .peek(rollerShutter -> log.info("Open RollerShutter " + rollerShutter.getName() + ". It is not so bright anymore"))
+          .forEach(RollerShutter::open);
+    } else if (newBrightness == 0) {
+      rollerShutterConfigRepository.findRollerShutterConfigByCompassDirectionIsNotNull()
+          .map(this::getRollerShutter)
+          .filter(RollerShutter::isNotCompletelyClosed)
+          .peek(rollerShutter -> log.info("Close RollerShutter " + rollerShutter.getName() + " because it is night."))
+          .forEach(RollerShutter::close);
+    }
   }
 
   private void initTimer() {
@@ -129,70 +154,10 @@ public class RollerShutterService {
     disposables.forEach(Disposable::dispose);
   }
 
-
-  private void itsGettingBrighterOutside(RollerShutterConfig config,
-      WeatherService.CurrentAndPreviousWeather weather) {
-    var rollerShutter = getRollerShutter(config);
-
-    if (rollerShutter.getCurrentLift() > 50
-        && shouldCloseBecauseOfSun(weather, config.getCompassDirection())
-        && weather.current().getOuterTemperatur().isBiggerThan(15, DEGREE)) {
-      log.info("Weather close for {} because it is too much sun", rollerShutter.getName());
-      rollerShutter.setLiftAndThenTilt(50, 30);
-    } else if (!rollerShutter.isOpen()
-        && brightnessIsGoingAboveThreshold(weather, 50)
-        && !config.isIgnoreWeatherInTheMorning()) {
-      log.info("Weather open for {}", rollerShutter.getName());
-      rollerShutter.open();
-    }
-  }
-
-  private boolean shouldCloseBecauseOfSun(WeatherService.CurrentAndPreviousWeather weather,
-      CompassDirection directionOfRollerShutter) {
-
-    if (!brightnessIsGoingAboveThreshold(weather, 100)) {
-      return false;
-    }
-
-    var sunDirection = weatherService.getCurrentSunDirection();
-    return sunDirection.getZenithAngle() < 55
-        && resolveCompassDirection(sunDirection).equals(directionOfRollerShutter);
-  }
-
   private CompassDirection resolveCompassDirection(AzimuthZenithAngle sunDirection) {
     return Arrays.stream(CompassDirection.values())
         .min(Comparator.comparing(value -> Math.abs(value.getDirection() - sunDirection.getAzimuth())))
         .orElseThrow(() -> new NoSuchElementException("Can't resolve direction for %s".formatted(sunDirection)));
-  }
-
-  private void itsGettingDarkerOutside(RollerShutterConfig config,
-      WeatherService.CurrentAndPreviousWeather weather) {
-    var rollerShutter = getRollerShutter(config);
-
-    if (rollerShutter.isNotCompletelyClosed()
-        && brightnessIsGoingBelowThreshold(weather, 0)
-        && !config.isIgnoreWeatherInTheEvening()) {
-      log.info("Weather close for {} because it is getting dark", rollerShutter.getName());
-      rollerShutter.close();
-    } else if (!rollerShutter.isOpen() && brightnessIsGoingBelowThreshold(weather, 150)) {
-      log.info("Weather open for {} because it is not longer super-bright outside", rollerShutter.getName());
-      rollerShutter.open();
-    }
-  }
-
-  @SuppressWarnings("SameParameterValue")
-  private boolean brightnessIsGoingAboveThreshold(WeatherService.CurrentAndPreviousWeather weather, int threshold) {
-    var current = weather.current().getLight().getAs(KILO_LUX).intValue();
-    var previous = weather.previous().getLight().getAs(KILO_LUX).intValue();
-
-    return current >= threshold && previous < threshold;
-  }
-
-  private boolean brightnessIsGoingBelowThreshold(WeatherService.CurrentAndPreviousWeather weather, int threshold) {
-    var current = weather.current().getLight().getAs(KILO_LUX).intValue();
-    var previous = weather.previous().getLight().getAs(KILO_LUX).intValue();
-
-    return current <= threshold && previous > threshold;
   }
 
   private RollerShutter getRollerShutter(RollerShutterConfig config) {
