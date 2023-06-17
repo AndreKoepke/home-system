9cd32c6126d9792f0a5c5814bb6bdc9f8d92f211
Andre Köpke <andre.koepke@sbb.ch>
2023-06-17 15:27:30 +0200

    higher tilt

--- backend/src/main/java/ch/akop/homesystem/services/impl/RollerShutterService.java
+++ backend/src/main/java/ch/akop/homesystem/services/impl/RollerShutterService.java
@@ -85,7 +85,7 @@ public class RollerShutterService {
                   if (sunDirection.getZenithAngle() > 40) {
                     return rollerShutter.setLiftAndThenTilt(50, 50);
                   } else if (sunDirection.getZenithAngle() > 20) {
-                    return rollerShutter.setLiftAndThenTilt(75, 50);
+                    return rollerShutter.setLiftAndThenTilt(75, 75);
                   }
                 }
                 log.info("Weather open for " + rollerShutter.getName() + " because sun is coming from other direction");
