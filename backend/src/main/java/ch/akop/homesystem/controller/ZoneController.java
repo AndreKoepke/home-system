492f5f9b003c08cd3d772b6d12ad740ba148cc28
e542445 <andre.koepke@sbb.ch>
2024-04-12 21:04:28 +0200

    endpoint

--- backend/src/main/java/ch/akop/homesystem/controller/ZoneController.java
+++ backend/src/main/java/ch/akop/homesystem/controller/ZoneController.java
@@ -1,9 +1,11 @@
 package ch.akop.homesystem.controller;
 
 import javax.annotation.security.PermitAll;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,5 +19,21 @@ public class ZoneController {
   @PermitAll
   public void enteredZone(@PathParam("apikey") String apikey, @PathParam("zoneId") String zoneId) {
     log.info("apiKey=" + apikey + " zoneId=" + zoneId);
+  }
+
+
+  @RequiredArgsConstructor
+  @Getter
+  static class demo {
+
+    private final int code;
+  }
+
+  @Path("test")
+  @GET
+  @PermitAll
+  public demo test(String payload) {
+    log.info(payload);
+    return new demo(0);
   }
 }
