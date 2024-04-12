dd89eae7f4827044dd1994bcc8e6fd7d9f323236
e542445 <andre.koepke@sbb.ch>
2024-04-12 21:09:01 +0200

    as text?

--- backend/src/main/java/ch/akop/homesystem/controller/ZoneController.java
+++ backend/src/main/java/ch/akop/homesystem/controller/ZoneController.java
@@ -5,7 +5,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,18 +21,11 @@ public class ZoneController {
   }
 
 
-  @RequiredArgsConstructor
-  @Getter
-  static class demo {
-
-    private final int code;
-  }
-
   @Path("test")
   @GET
   @PermitAll
-  public demo test(String payload) {
+  public String test(String payload) {
     log.info(payload);
-    return new demo(0);
+    return "{'code': 0}";
   }
 }
