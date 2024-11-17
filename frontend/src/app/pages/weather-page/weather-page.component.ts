bdbc0d250ded9aaa5e2958234b405484f7c1256f
e542445 <andre.koepke@sbb.ch>
2024-11-17 16:25:13 +0100

    profile for weater

--- frontend/src/app/pages/weather-page/weather-page.component.ts
+++ frontend/src/app/pages/weather-page/weather-page.component.ts
@@ -1,6 +1,4 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
-import {interval} from "rxjs";
-import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-weather-page',
@@ -10,36 +8,5 @@ import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
   styleUrl: './weather-page.component.scss'
 })
 export class WeatherPageComponent {
-
-  @ViewChild('container')
-  private iframe: ElementRef | undefined;
-
-  public startScrolling(): void {
-    let current = 0;
-    let toUpper = true;
-    interval(5000)
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => {
-        const maxHeight = this.iframe?.nativeElement.scrollHeight;
-
-
-        if ((current + maxHeight * 0.3) > maxHeight) {
-          toUpper = false;
-        }
-
-        if ((current - maxHeight * 0.3) <= 0) {
-          toUpper = true;
-        }
-
-        if (toUpper) {
-          current += maxHeight * 0.3;
-        } else {
-          current -= maxHeight * 0.3;
-        }
-
-        this.iframe?.nativeElement.scrollTo(0, current);
-        console.log(current)
-      });
-  }
 
 }
