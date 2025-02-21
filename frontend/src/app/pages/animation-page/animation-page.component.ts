f1c9df1347483ca6cf10a0f33d02a4bd8337a246
e542445 <andre.koepke@sbb.ch>
2025-02-21 18:52:05 +0100

    better styling

--- frontend/src/app/pages/animation-page/animation-page.component.ts
+++ frontend/src/app/pages/animation-page/animation-page.component.ts
@@ -1,10 +1,13 @@
 import {Component, input, Output, OutputEmitterRef} from '@angular/core';
 import {AnimationDto} from "../../models/animation.dto";
+import {CircleButtonComponent} from "../../components/buttons/circle-button/circle-button.component";
 
 @Component({
   selector: 'app-animation-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CircleButtonComponent
+  ],
   templateUrl: './animation-page.component.html',
   styleUrl: './animation-page.component.scss'
 })
