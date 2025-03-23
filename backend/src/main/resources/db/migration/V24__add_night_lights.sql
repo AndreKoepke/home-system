b26fa8c0b6cae95d808242fb889ea694b81d6271
e542445 <andre.koepke@sbb.ch>
2025-03-23 14:13:47 +0100

    fix drop invalid primary key

--- backend/src/main/resources/db/migration/V24__add_night_lights.sql
+++ backend/src/main/resources/db/migration/V24__add_night_lights.sql
@@ -1,6 +1,5 @@
 ALTER TABLE config_motion_sensor
-  ADD COLUMN IF NOT EXISTS animation_id_night uuid NULL REFERENCES animation (id),
-  ADD PRIMARY KEY (name);
+  ADD COLUMN IF NOT EXISTS animation_id_night uuid NULL REFERENCES animation (id);
 
 CREATE TABLE IF NOT EXISTS config_motion_sensor_lights_night
 (
