95a1eb4dab6548df956712a1638a4ad7e0fc3b86
e542445 <andre.koepke@sbb.ch>
2025-03-23 14:05:26 +0100

    savepoint

---
+++ frontend/src/app/pages/animation-page/animation-editor/animation-editor.component.stories.ts
@@ -0,0 +1,34 @@
+import type {Meta, StoryObj} from '@storybook/angular';
+import {AnimationEditorComponent} from "./animation-editor.component";
+import {AnimationDto} from "../../../models/animation.dto";
+
+
+//👇 This default export determines where your story goes in the story list
+const meta: Meta<AnimationEditorComponent> = {
+  component: AnimationEditorComponent,
+};
+
+export default meta;
+type Story = StoryObj<AnimationEditorComponent>;
+
+export const EmptyAnimation: Story = {
+  args: {
+    animation: {id: 'empty', steps: [], name: 'test'} as AnimationDto
+  },
+};
+
+
+export const SingleStep: Story = {
+  args: {
+    animation: {
+      id: 'empty',
+      name: 'test',
+      steps: [{
+        id: 'firstStep',
+        sortOrder: 1,
+        actionDescription: 'Allet aus',
+        affectedLight: 'Allet'
+      }]
+    } as AnimationDto
+  },
+};
