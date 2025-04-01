278b768ec26fd4eccb3c9ba2b7316fbfe3389417
e542445 <andre.koepke@sbb.ch>
2025-04-01 08:19:08 +0200

    improve zenith

--- backend/src/main/java/ch/akop/homesystem/services/impl/RollerShutterService.java
+++ backend/src/main/java/ch/akop/homesystem/services/impl/RollerShutterService.java
@@ -220,10 +220,12 @@ public class RollerShutterService {
 
   private Completable openBasedOnZenithAngle(RollerShutter rollerShutter, double zenithAngle) {
     var config = rollerShutter.getConfig();
-    if (zenithAngle > 40) {
-      return rollerShutter.setLiftAndThenTilt(config.getCloseLevelLowLift(), config.getCloseLevelLowTilt(), "brightness");
-    } else if (zenithAngle > 20) {
+    if (zenithAngle > 70) {
+      return rollerShutter.setLiftAndThenTilt(0, config.getCloseLevelHighTilt(), "brightness and high zenith angle");
+    } else if (zenithAngle > 40) {
       return rollerShutter.setLiftAndThenTilt(config.getCloseLevelHighLift(), config.getCloseLevelHighTilt(), "brightness");
+    } else if (zenithAngle > 20) {
+      return rollerShutter.setLiftAndThenTilt(config.getCloseLevelLowLift(), config.getCloseLevelLowTilt(), "brightness");
     }
 
     return Completable.complete();
