4fffbe21ff700d068b153925f7a181985fa3a188
e542445 <andre.koepke@sbb.ch>
2026-04-01 19:52:10 +0200

    serialization

--- backend/src/main/java/ch/akop/homesystem/config/JacksonConfig.java
+++ backend/src/main/java/ch/akop/homesystem/config/JacksonConfig.java
@@ -3,6 +3,7 @@ package ch.akop.homesystem.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.quarkus.jackson.ObjectMapperCustomizer;
 import jakarta.inject.Singleton;
 
@@ -13,6 +14,7 @@ public class JacksonConfig implements ObjectMapperCustomizer {
   public void customize(ObjectMapper objectMapper) {
     objectMapper
         .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
+        .registerModule(new JavaTimeModule())
         .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
   }
 }
